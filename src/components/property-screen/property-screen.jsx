9558c5a1128f929003c077efee2606228c3534b7
Dedenich <sergbrst@tut.by>
2020-07-03 14:05:02 +0300

    Внесет изменения

--- src/components/property-screen/property-screen.jsx
+++ src/components/property-screen/property-screen.jsx
@@ -1,6 +1,5 @@
 import React, {PureComponent} from "react";
-import PropTypes from 'prop-types';
-import TYPES from '../../const.js';
+import pt from "./property-screen-pt.jsx";
 
 export default class PropretyScreen extends PureComponent {
   constructor(props) {
@@ -288,21 +287,4 @@ export default class PropretyScreen extends PureComponent {
   }
 }
 
-PropretyScreen.propTypes = {
-  photos: PropTypes.arrayOf(PropTypes.string).isRequired,
-  heading: PropTypes.string.isRequired,
-  describtion: PropTypes.string.isRequired,
-  premium: PropTypes.bool.isRequired,
-  type: PropTypes.oneOf(TYPES).isRequired,
-  rating: PropTypes.number.isRequired,
-  bedroomsNumber: PropTypes.number.isRequired,
-  maxGuests: PropTypes.number.isRequired,
-  price: PropTypes.number.isRequired,
-  list: PropTypes.arrayOf(PropTypes.string).isRequired,
-  host: PropTypes.shape({
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    superStatus: PropTypes.bool.isRequired,
-  }).isRequired,
-};
-
+PropretyScreen.propTypes = pt;
