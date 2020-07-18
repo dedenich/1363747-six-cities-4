119fdd9d729d834cd13342f9110bf2c3d92b539b
Dedenich <sergbrst@tut.by>
2020-07-19 00:03:33 +0300

    Удалит неиспольузуемые переменные из теста

--- src/components/offers-list/offers-list.test.js
+++ src/components/offers-list/offers-list.test.js
@@ -4,30 +4,9 @@ import OffersList from './offers-list.jsx';
 import {Provider} from 'react-redux';
 import configureStore from "redux-mock-store";
 
-import NameSpace from '../../reducers/namespace.js';
-import {AuthorizationStatus} from "../../const.js";
-
 import mockStoreConf from "../../mocks/mock-store-conf.js";
 
 const mockStore = configureStore([]);
-
-const mockOffersList = [
-  {
-    caption: `Capion 1`,
-    src: `https://path`,
-    price: 100
-  },
-  {
-    caption: `Capion 2`,
-    src: `https://path`,
-    price: 160
-  },
-  {
-    caption: `Capion 3`,
-    src: `https://path`,
-    price: 90
-  },
-];
 
 it(`renders correctly`, () => {
   const store = mockStore(mockStoreConf);
