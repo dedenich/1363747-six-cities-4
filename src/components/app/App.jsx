e0b860f707be6061faf7e4d37f7f78d75cd02bd8
Dedenich <sergbrst@tut.by>
2020-07-24 13:33:13 +0300

    Вернет PropertyScreen

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,6 +7,7 @@ import history from "../../history.js";
 import {AppRoute} from "../../const.js";
 
 import Main from "../main/Main.jsx";
+import PropertyScreen from "../property-screen/property-screen.jsx";
 import LoginScreen from "../login-screen/login-screen.jsx";
 
 import {Operation as OfferOperation} from "../../reducers/offers/offers.js";
@@ -41,7 +42,7 @@ class App extends PureComponent {
           <Route path={AppRoute.ROOT} component={Main} exact />
           <Route path={AppRoute.SING_IN} component={LoginScreen} exact />
           <Route path={AppRoute.FAVORITES} exact />
-          <Route path={AppRoute.ROOM} exact />
+          <Route path={AppRoute.ROOM} exact component={PropertyScreen}/>
 
         </Switch>
       </Router>
